2f3f0d31c9048beb268006275ca80c0dd1f0e4d5
xconstaud <xconstaud@gmail.com>
2017-05-09 22:19:47 -0500

    Add bulk footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,6 +11,7 @@
 	</head>
 	<body>
 	@yield('cover')
+	<!-- Navbar -->
 		<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -53,7 +54,35 @@
 			</div>
 		</nav>
 
+	
+
 	@yield('content')
+	<!-- Footer -->
+		<div class="footer">
+			<div class="container-fluid">
+				<div class="row topPaddingSm">
+					<div class="col-md-3 col-sm-12 offset-sm-0">
+						<h3>WHAT WE'RE ABOUT</h3>
+						<p>We are about giving the power back to the creator... rather they are experienced or just getting started. We provide the tools needed to make it happen. Build your dream.</p>
+					</div>
+					<div class="col-md-3 offset-md-2 col-sm-12 offset-sm-0">
+						<h3>NEED HELP?</h3>
+						<ul>
+							<li><a href="">Visual Tour</a></li>
+							<li><a href="">FAQs</a></li>
+							<li><a href="">Contact Us</a></li>
+							<li><a href="">Privacy</a></li>
+							<li><a href="">Terms</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3 offset-md-1 col-sm-12 offset-sm-0">
+						<img src="/images/mountainLogo.png" alt="Olympus Logo">
+						<h3>OLYMPUS PC BUILDER</h3>
+						<p class="forceWrap">contact@olympuspcbuilder.com</p>
+					</div>
+				</div>
+			</div>
+		</div>
 
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
