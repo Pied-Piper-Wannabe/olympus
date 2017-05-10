89e06774a355d550c666917b660f7c15212a6a98
gtw117 <gtw117@users.noreply.github.com>
2017-05-10 15:28:34 -0500

    case_table initial, users/password_resets fix indentation

--- database/migrations/2017_05_10_201945_create_case_table.php
+++ database/migrations/2017_05_10_201945_create_case_table.php
@@ -5,23 +5,25 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateCaseTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        //
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('case', function(Blueprint $table){
+			$table->
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
 }
