196c690436ea7114e2497e60faea8de2ddd94dc5
Connor <Csumners17@gmail.com>
2017-05-10 15:02:31 -0500

    Add boilerplate seeder for mobos

---
+++ database/seeds/MoboTableSeeder.php
@@ -0,0 +1,457 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class MoboTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $mobo = new \App\Models\Post();
+        $mobo->model =
+        $mobo->brand =
+        $mobo->manufacturer =
+        $mobo->cpu_socket_type =
+        $mobo->cpu_type =
+        $mobo->supported_cpu_technology =
+        $mobo->chipset =
+        $mobo->onboard_gpu =
+        $mobo->onboard_audio =
+        $mobo->onboard_lan =
+        $mobo->memory_slot_number =
+        $mobo->max_memory =
+        $mobo->channel_support =
+        $mobo->expansion_slots =
+        $mobo->storage_devices =
+        $mobo->rear_panel_ports =
+        $mobo->io_connectors =
+        $mobo->form_factor =
+        $mobo->save();
+
+        $mobo1 = new \App\Models\Post();
+        $mobo1->model =
+        $mobo1->brand =
+        $mobo1->manufacturer =
+        $mobo1->cpu_socket_type =
+        $mobo1->cpu_type =
+        $mobo1->supported_cpu_technology =
+        $mobo1->chipset =
+        $mobo1->onboard_gpu =
+        $mobo1->onboard_audio =
+        $mobo1->onboard_lan =
+        $mobo1->memory_slot_number =
+        $mobo1->max_memory =
+        $mobo1->channel_support =
+        $mobo1->expansion_slots =
+        $mobo1->storage_devices =
+        $mobo1->rear_panel_ports =
+        $mobo1->io_connectors =
+        $mobo1->form_factor =
+        $mobo1->save();
+
+        $mobo2 = new \App\Models\Post();
+        $mobo2->model =
+        $mobo2->brand =
+        $mobo2->manufacturer =
+        $mobo2->cpu_socket_type =
+        $mobo2->cpu_type =
+        $mobo2->supported_cpu_technology =
+        $mobo2->chipset =
+        $mobo2->onboard_gpu =
+        $mobo2->onboard_audio =
+        $mobo2->onboard_lan =
+        $mobo2->memory_slot_number =
+        $mobo2->max_memory =
+        $mobo2->channel_support =
+        $mobo2->expansion_slots =
+        $mobo2->storage_devices =
+        $mobo2->rear_panel_ports =
+        $mobo2->io_connectors =
+        $mobo2->form_factor =
+        $mobo2->save();
+
+        $mobo3 = new \App\Models\Post();
+        $mobo3->model =
+        $mobo3->brand =
+        $mobo3->manufacturer =
+        $mobo3->cpu_socket_type =
+        $mobo3->cpu_type =
+        $mobo3->supported_cpu_technology =
+        $mobo3->chipset =
+        $mobo3->onboard_gpu =
+        $mobo3->onboard_audio =
+        $mobo3->onboard_lan =
+        $mobo3->memory_slot_number =
+        $mobo3->max_memory =
+        $mobo3->channel_support =
+        $mobo3->expansion_slots =
+        $mobo3->storage_devices =
+        $mobo3->rear_panel_ports =
+        $mobo3->io_connectors =
+        $mobo3->form_factor =
+        $mobo3->save();
+
+        $mobo4 = new \App\Models\Post();
+        $mobo4->model =
+        $mobo4->brand =
+        $mobo4->manufacturer =
+        $mobo4->cpu_socket_type =
+        $mobo4->cpu_type =
+        $mobo4->supported_cpu_technology =
+        $mobo4->chipset =
+        $mobo4->onboard_gpu =
+        $mobo4->onboard_audio =
+        $mobo4->onboard_lan =
+        $mobo4->memory_slot_number =
+        $mobo4->max_memory =
+        $mobo4->channel_support =
+        $mobo4->expansion_slots =
+        $mobo4->storage_devices =
+        $mobo4->rear_panel_ports =
+        $mobo4->io_connectors =
+        $mobo4->form_factor =
+        $mobo4->save();
+
+        $mobo5 = new \App\Models\Post();
+        $mobo5->model =
+        $mobo5->brand =
+        $mobo5->manufacturer =
+        $mobo5->cpu_socket_type =
+        $mobo5->cpu_type =
+        $mobo5->supported_cpu_technology =
+        $mobo5->chipset =
+        $mobo5->onboard_gpu =
+        $mobo5->onboard_audio =
+        $mobo5->onboard_lan =
+        $mobo5->memory_slot_number =
+        $mobo5->max_memory =
+        $mobo5->channel_support =
+        $mobo5->expansion_slots =
+        $mobo5->storage_devices =
+        $mobo5->rear_panel_ports =
+        $mobo5->io_connectors =
+        $mobo5->form_factor =
+        $mobo5->save();
+
+        $mobo6 = new \App\Models\Post();
+        $mobo6->model =
+        $mobo6->brand =
+        $mobo6->manufacturer =
+        $mobo6->cpu_socket_type =
+        $mobo6->cpu_type =
+        $mobo6->supported_cpu_technology =
+        $mobo6->chipset =
+        $mobo6->onboard_gpu =
+        $mobo6->onboard_audio =
+        $mobo6->onboard_lan =
+        $mobo6->memory_slot_number =
+        $mobo6->max_memory =
+        $mobo6->channel_support =
+        $mobo6->expansion_slots =
+        $mobo6->storage_devices =
+        $mobo6->rear_panel_ports =
+        $mobo6->io_connectors =
+        $mobo6->form_factor =
+        $mobo6->save();
+
+        $mobo7 = new \App\Models\Post();
+        $mobo7->model =
+        $mobo7->brand =
+        $mobo7->manufacturer =
+        $mobo7->cpu_socket_type =
+        $mobo7->cpu_type =
+        $mobo7->supported_cpu_technology =
+        $mobo7->chipset =
+        $mobo7->onboard_gpu =
+        $mobo7->onboard_audio =
+        $mobo7->onboard_lan =
+        $mobo7->memory_slot_number =
+        $mobo7->max_memory =
+        $mobo7->channel_support =
+        $mobo7->expansion_slots =
+        $mobo7->storage_devices =
+        $mobo7->rear_panel_ports =
+        $mobo7->io_connectors =
+        $mobo7->form_factor =
+        $mobo7->save();
+
+        $mobo8 = new \App\Models\Post();
+        $mobo8->model =
+        $mobo8->brand =
+        $mobo8->manufacturer =
+        $mobo8->cpu_socket_type =
+        $mobo8->cpu_type =
+        $mobo8->supported_cpu_technology =
+        $mobo8->chipset =
+        $mobo8->onboard_gpu =
+        $mobo8->onboard_audio =
+        $mobo8->onboard_lan =
+        $mobo8->memory_slot_number =
+        $mobo8->max_memory =
+        $mobo8->channel_support =
+        $mobo8->expansion_slots =
+        $mobo8->storage_devices =
+        $mobo8->rear_panel_ports =
+        $mobo8->io_connectors =
+        $mobo8->form_factor =
+        $mobo8->save();
+
+        $mobo9 = new \App\Models\Post();
+        $mobo9->model =
+        $mobo9->brand =
+        $mobo9->manufacturer =
+        $mobo9->cpu_socket_type =
+        $mobo9->cpu_type =
+        $mobo9->supported_cpu_technology =
+        $mobo9->chipset =
+        $mobo9->onboard_gpu =
+        $mobo9->onboard_audio =
+        $mobo9->onboard_lan =
+        $mobo9->memory_slot_number =
+        $mobo9->max_memory =
+        $mobo9->channel_support =
+        $mobo9->expansion_slots =
+        $mobo9->storage_devices =
+        $mobo9->rear_panel_ports =
+        $mobo9->io_connectors =
+        $mobo9->form_factor =
+        $mobo9->save();
+
+        $mobo10 = new \App\Models\Post();
+        $mobo10->model =
+        $mobo10->brand =
+        $mobo10->manufacturer =
+        $mobo10->cpu_socket_type =
+        $mobo10->cpu_type =
+        $mobo10->supported_cpu_technology =
+        $mobo10->chipset =
+        $mobo10->onboard_gpu =
+        $mobo10->onboard_audio =
+        $mobo10->onboard_lan =
+        $mobo10->memory_slot_number =
+        $mobo10->max_memory =
+        $mobo10->channel_support =
+        $mobo10->expansion_slots =
+        $mobo10->storage_devices =
+        $mobo10->rear_panel_ports =
+        $mobo10->io_connectors =
+        $mobo10->form_factor =
+        $mobo10->save();
+
+        $mobo11 = new \App\Models\Post();
+        $mobo11->model =
+        $mobo11->brand =
+        $mobo11->manufacturer =
+        $mobo11->cpu_socket_type =
+        $mobo11->cpu_type =
+        $mobo11->supported_cpu_technology =
+        $mobo11->chipset =
+        $mobo11->onboard_gpu =
+        $mobo11->onboard_audio =
+        $mobo11->onboard_lan =
+        $mobo11->memory_slot_number =
+        $mobo11->max_memory =
+        $mobo11->channel_support =
+        $mobo11->expansion_slots =
+        $mobo11->storage_devices =
+        $mobo11->rear_panel_ports =
+        $mobo11->io_connectors =
+        $mobo11->form_factor =
+        $mobo11->save();
+
+        $mobo12 = new \App\Models\Post();
+        $mobo12->model =
+        $mobo12->brand =
+        $mobo12->manufacturer =
+        $mobo12->cpu_socket_type =
+        $mobo12->cpu_type =
+        $mobo12->supported_cpu_technology =
+        $mobo12->chipset =
+        $mobo12->onboard_gpu =
+        $mobo12->onboard_audio =
+        $mobo12->onboard_lan =
+        $mobo12->memory_slot_number =
+        $mobo12->max_memory =
+        $mobo12->channel_support =
+        $mobo12->expansion_slots =
+        $mobo12->storage_devices =
+        $mobo12->rear_panel_ports =
+        $mobo12->io_connectors =
+        $mobo12->form_factor =
+        $mobo12->save();
+
+        $mobo13 = new \App\Models\Post();
+        $mobo13->model =
+        $mobo13->brand =
+        $mobo13->manufacturer =
+        $mobo13->cpu_socket_type =
+        $mobo13->cpu_type =
+        $mobo13->supported_cpu_technology =
+        $mobo13->chipset =
+        $mobo13->onboard_gpu =
+        $mobo13->onboard_audio =
+        $mobo13->onboard_lan =
+        $mobo13->memory_slot_number =
+        $mobo13->max_memory =
+        $mobo13->channel_support =
+        $mobo13->expansion_slots =
+        $mobo13->storage_devices =
+        $mobo13->rear_panel_ports =
+        $mobo13->io_connectors =
+        $mobo13->form_factor =
+        $mobo13->save();
+
+        $mobo14 = new \App\Models\Post();
+        $mobo14->model =
+        $mobo14->brand =
+        $mobo14->manufacturer =
+        $mobo14->cpu_socket_type =
+        $mobo14->cpu_type =
+        $mobo14->supported_cpu_technology =
+        $mobo14->chipset =
+        $mobo14->onboard_gpu =
+        $mobo14->onboard_audio =
+        $mobo14->onboard_lan =
+        $mobo14->memory_slot_number =
+        $mobo14->max_memory =
+        $mobo14->channel_support =
+        $mobo14->expansion_slots =
+        $mobo14->storage_devices =
+        $mobo14->rear_panel_ports =
+        $mobo14->io_connectors =
+        $mobo14->form_factor =
+        $mobo14->save();
+
+        $mobo15 = new \App\Models\Post();
+        $mobo15->model =
+        $mobo15->brand =
+        $mobo15->manufacturer =
+        $mobo15->cpu_socket_type =
+        $mobo15->cpu_type =
+        $mobo15->supported_cpu_technology =
+        $mobo15->chipset =
+        $mobo15->onboard_gpu =
+        $mobo15->onboard_audio =
+        $mobo15->onboard_lan =
+        $mobo15->memory_slot_number =
+        $mobo15->max_memory =
+        $mobo15->channel_support =
+        $mobo15->expansion_slots =
+        $mobo15->storage_devices =
+        $mobo15->rear_panel_ports =
+        $mobo15->io_connectors =
+        $mobo15->form_factor =
+        $mobo15->save();
+
+        $mobo16 = new \App\Models\Post();
+        $mobo16->model =
+        $mobo16->brand =
+        $mobo16->manufacturer =
+        $mobo16->cpu_socket_type =
+        $mobo16->cpu_type =
+        $mobo16->supported_cpu_technology =
+        $mobo16->chipset =
+        $mobo16->onboard_gpu =
+        $mobo16->onboard_audio =
+        $mobo16->onboard_lan =
+        $mobo16->memory_slot_number =
+        $mobo16->max_memory =
+        $mobo16->channel_support =
+        $mobo16->expansion_slots =
+        $mobo16->storage_devices =
+        $mobo16->rear_panel_ports =
+        $mobo16->io_connectors =
+        $mobo16->form_factor =
+        $mobo16->save();
+
+        $mobo17 = new \App\Models\Post();
+        $mobo17->model =
+        $mobo17->brand =
+        $mobo17->manufacturer =
+        $mobo17->cpu_socket_type =
+        $mobo17->cpu_type =
+        $mobo17->supported_cpu_technology =
+        $mobo17->chipset =
+        $mobo17->onboard_gpu =
+        $mobo17->onboard_audio =
+        $mobo17->onboard_lan =
+        $mobo17->memory_slot_number =
+        $mobo17->max_memory =
+        $mobo17->channel_support =
+        $mobo17->expansion_slots =
+        $mobo17->storage_devices =
+        $mobo17->rear_panel_ports =
+        $mobo17->io_connectors =
+        $mobo17->form_factor =
+        $mobo17->save();
+
+        $mobo18 = new \App\Models\Post();
+        $mobo18->model =
+        $mobo18->brand =
+        $mobo18->manufacturer =
+        $mobo18->cpu_socket_type =
+        $mobo18->cpu_type =
+        $mobo18->supported_cpu_technology =
+        $mobo18->chipset =
+        $mobo18->onboard_gpu =
+        $mobo18->onboard_audio =
+        $mobo18->onboard_lan =
+        $mobo18->memory_slot_number =
+        $mobo18->max_memory =
+        $mobo18->channel_support =
+        $mobo18->expansion_slots =
+        $mobo18->storage_devices =
+        $mobo18->rear_panel_ports =
+        $mobo18->io_connectors =
+        $mobo18->form_factor =
+        $mobo18->save();
+
+        $mobo19 = new \App\Models\Post();
+        $mobo19->model =
+        $mobo19->brand =
+        $mobo19->manufacturer =
+        $mobo19->cpu_socket_type =
+        $mobo19->cpu_type =
+        $mobo19->supported_cpu_technology =
+        $mobo19->chipset =
+        $mobo19->onboard_gpu =
+        $mobo19->onboard_audio =
+        $mobo19->onboard_lan =
+        $mobo19->memory_slot_number =
+        $mobo19->max_memory =
+        $mobo19->channel_support =
+        $mobo19->expansion_slots =
+        $mobo19->storage_devices =
+        $mobo19->rear_panel_ports =
+        $mobo19->io_connectors =
+        $mobo19->form_factor =
+        $mobo19->save();
+
+        $mobo20 = new \App\Models\Post();
+        $mobo20->model =
+        $mobo20->brand =
+        $mobo20->manufacturer =
+        $mobo20->cpu_socket_type =
+        $mobo20->cpu_type =
+        $mobo20->supported_cpu_technology =
+        $mobo20->chipset =
+        $mobo20->onboard_gpu =
+        $mobo20->onboard_audio =
+        $mobo20->onboard_lan =
+        $mobo20->memory_slot_number =
+        $mobo20->max_memory =
+        $mobo20->channel_support =
+        $mobo20->expansion_slots =
+        $mobo20->storage_devices =
+        $mobo20->rear_panel_ports =
+        $mobo20->io_connectors =
+        $mobo20->form_factor =
+        $mobo20->save();
+
+    }
+}
