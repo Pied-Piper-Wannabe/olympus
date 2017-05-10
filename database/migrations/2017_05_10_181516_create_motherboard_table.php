3a858d46db869cb7013f71e6d64ee499b6899da7
gtw117 <gtw117@users.noreply.github.com>
2017-05-10 14:55:59 -0500

    refactor motherboard_table, cpu_table initial

--- database/migrations/2017_05_10_181516_create_motherboard_table.php
+++ database/migrations/2017_05_10_181516_create_motherboard_table.php
@@ -5,27 +5,25 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateMotherboardTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('motherboard', function(Blueprint $table){
-            $table->string('partname');
-            $table->decimal('price', 5, 2);
-            $table->
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('motherboard', function(Blueprint $table){
+			$table->
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
 }
