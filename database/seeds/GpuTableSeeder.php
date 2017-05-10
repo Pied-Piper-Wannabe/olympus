659ab26373eea86aba9ef99b113f517a4b309b1d
Connor <Csumners17@gmail.com>
2017-05-10 14:54:04 -0500

    Add boilerplate seeder for GPUs

---
+++ database/seeds/GpuTableSeeder.php
@@ -0,0 +1,520 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class GpuTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $gpu = new \App\Models\Post();
+        $gpu->model =
+        $gpu->brand =
+        $gpu->interface =
+        $gpu->manufacturer =
+        $gpu->core_clock =
+        $gpu->boost_clock =
+        $gpu->cuda_cores =
+        $gpu->memory_clock =
+        $gpu->memory_size =
+        $gpu->memory_interface =
+        $gpu->memory_type =
+        $gpu->api_3d =
+        $gpu->hdmi =
+        $gpu->multi_monitor_support =
+        $gpu->display_port =
+        $gpu->dvi =
+        $gpu->sli_support =
+        $gpu->system_requirements =
+        $gpu->power_connector
+        $gpu->dual_link_dvi_support =
+        $gpu->hdcp_ready =
+        $gpu->save();
+
+        $gpu1 = new \App\Models\Post();
+        $gpu1->model =
+        $gpu1->brand =
+        $gpu1->interface =
+        $gpu1->manufacturer =
+        $gpu1->core_clock =
+        $gpu1->boost_clock =
+        $gpu1->cuda_cores =
+        $gpu1->memory_clock =
+        $gpu1->memory_size =
+        $gpu1->memory_interface =
+        $gpu1->memory_type =
+        $gpu1->api_3d =
+        $gpu1->hdmi =
+        $gpu1->multi_monitor_support =
+        $gpu1->display_port =
+        $gpu1->dvi =
+        $gpu1->sli_support =
+        $gpu1->system_requirements =
+        $gpu1->power_connector
+        $gpu1->dual_link_dvi_support =
+        $gpu1->hdcp_ready =
+        $gpu1->save();
+
+        $gpu2 = new \App\Models\Post();
+        $gpu2->model =
+        $gpu2->brand =
+        $gpu2->interface =
+        $gpu2->manufacturer =
+        $gpu2->core_clock =
+        $gpu2->boost_clock =
+        $gpu2->cuda_cores =
+        $gpu2->memory_clock =
+        $gpu2->memory_size =
+        $gpu2->memory_interface =
+        $gpu2->memory_type =
+        $gpu2->api_3d =
+        $gpu2->hdmi =
+        $gpu2->multi_monitor_support =
+        $gpu2->display_port =
+        $gpu2->dvi =
+        $gpu2->sli_support =
+        $gpu2->system_requirements =
+        $gpu2->power_connector
+        $gpu2->dual_link_dvi_support =
+        $gpu2->hdcp_ready =
+        $gpu2->save();
+
+        $gpu3 = new \App\Models\Post();
+        $gpu3->model =
+        $gpu3->brand =
+        $gpu3->interface =
+        $gpu3->manufacturer =
+        $gpu3->core_clock =
+        $gpu3->boost_clock =
+        $gpu3->cuda_cores =
+        $gpu3->memory_clock =
+        $gpu3->memory_size =
+        $gpu3->memory_interface =
+        $gpu3->memory_type =
+        $gpu3->api_3d =
+        $gpu3->hdmi =
+        $gpu3->multi_monitor_support =
+        $gpu3->display_port =
+        $gpu3->dvi =
+        $gpu3->sli_support =
+        $gpu3->system_requirements =
+        $gpu3->power_connector
+        $gpu3->dual_link_dvi_support =
+        $gpu3->hdcp_ready =
+        $gpu3->save();
+
+        $gpu4 = new \App\Models\Post();
+        $gpu4->model =
+        $gpu4->brand =
+        $gpu4->interface =
+        $gpu4->manufacturer =
+        $gpu4->core_clock =
+        $gpu4->boost_clock =
+        $gpu4->cuda_cores =
+        $gpu4->memory_clock =
+        $gpu4->memory_size =
+        $gpu4->memory_interface =
+        $gpu4->memory_type =
+        $gpu4->api_3d =
+        $gpu4->hdmi =
+        $gpu4->multi_monitor_support =
+        $gpu4->display_port =
+        $gpu4->dvi =
+        $gpu4->sli_support =
+        $gpu4->system_requirements =
+        $gpu4->power_connector
+        $gpu4->dual_link_dvi_support =
+        $gpu4->hdcp_ready =
+        $gpu4->save();
+
+        $gpu5 = new \App\Models\Post();
+        $gpu5->model =
+        $gpu5->brand =
+        $gpu5->interface =
+        $gpu5->manufacturer =
+        $gpu5->core_clock =
+        $gpu5->boost_clock =
+        $gpu5->cuda_cores =
+        $gpu5->memory_clock =
+        $gpu5->memory_size =
+        $gpu5->memory_interface =
+        $gpu5->memory_type =
+        $gpu5->api_3d =
+        $gpu5->hdmi =
+        $gpu5->multi_monitor_support =
+        $gpu5->display_port =
+        $gpu5->dvi =
+        $gpu5->sli_support =
+        $gpu5->system_requirements =
+        $gpu5->power_connector
+        $gpu5->dual_link_dvi_support =
+        $gpu5->hdcp_ready =
+        $gpu5->save();
+
+        $gpu6 = new \App\Models\Post();
+        $gpu6->model =
+        $gpu6->brand =
+        $gpu6->interface =
+        $gpu6->manufacturer =
+        $gpu6->core_clock =
+        $gpu6->boost_clock =
+        $gpu6->cuda_cores =
+        $gpu6->memory_clock =
+        $gpu6->memory_size =
+        $gpu6->memory_interface =
+        $gpu6->memory_type =
+        $gpu6->api_3d =
+        $gpu6->hdmi =
+        $gpu6->multi_monitor_support =
+        $gpu6->display_port =
+        $gpu6->dvi =
+        $gpu6->sli_support =
+        $gpu6->system_requirements =
+        $gpu6->power_connector
+        $gpu6->dual_link_dvi_support =
+        $gpu6->hdcp_ready =
+        $gpu6->save();
+
+        $gpu7 = new \App\Models\Post();
+        $gpu7->model =
+        $gpu7->brand =
+        $gpu7->interface =
+        $gpu7->manufacturer =
+        $gpu7->core_clock =
+        $gpu7->boost_clock =
+        $gpu7->cuda_cores =
+        $gpu7->memory_clock =
+        $gpu7->memory_size =
+        $gpu7->memory_interface =
+        $gpu7->memory_type =
+        $gpu7->api_3d =
+        $gpu7->hdmi =
+        $gpu7->multi_monitor_support =
+        $gpu7->display_port =
+        $gpu7->dvi =
+        $gpu7->sli_support =
+        $gpu7->system_requirements =
+        $gpu7->power_connector
+        $gpu7->dual_link_dvi_support =
+        $gpu7->hdcp_ready =
+        $gpu7->save();
+
+        $gpu8 = new \App\Models\Post();
+        $gpu8->model =
+        $gpu8->brand =
+        $gpu8->interface =
+        $gpu8->manufacturer =
+        $gpu8->core_clock =
+        $gpu8->boost_clock =
+        $gpu8->cuda_cores =
+        $gpu8->memory_clock =
+        $gpu8->memory_size =
+        $gpu8->memory_interface =
+        $gpu8->memory_type =
+        $gpu8->api_3d =
+        $gpu8->hdmi =
+        $gpu8->multi_monitor_support =
+        $gpu8->display_port =
+        $gpu8->dvi =
+        $gpu8->sli_support =
+        $gpu8->system_requirements =
+        $gpu8->power_connector
+        $gpu8->dual_link_dvi_support =
+        $gpu8->hdcp_ready =
+        $gpu8->save();
+
+        $gpu9 = new \App\Models\Post();
+        $gpu9->model =
+        $gpu9->brand =
+        $gpu9->interface =
+        $gpu9->manufacturer =
+        $gpu9->core_clock =
+        $gpu9->boost_clock =
+        $gpu9->cuda_cores =
+        $gpu9->memory_clock =
+        $gpu9->memory_size =
+        $gpu9->memory_interface =
+        $gpu9->memory_type =
+        $gpu9->api_3d =
+        $gpu9->hdmi =
+        $gpu9->multi_monitor_support =
+        $gpu9->display_port =
+        $gpu9->dvi =
+        $gpu9->sli_support =
+        $gpu9->system_requirements =
+        $gpu9->power_connector
+        $gpu9->dual_link_dvi_support =
+        $gpu9->hdcp_ready =
+        $gpu9->save();
+
+        $gpu10 = new \App\Models\Post();
+        $gpu10->model =
+        $gpu10->brand =
+        $gpu10->interface =
+        $gpu10->manufacturer =
+        $gpu10->core_clock =
+        $gpu10->boost_clock =
+        $gpu10->cuda_cores =
+        $gpu10->memory_clock =
+        $gpu10->memory_size =
+        $gpu10->memory_interface =
+        $gpu10->memory_type =
+        $gpu10->api_3d =
+        $gpu10->hdmi =
+        $gpu10->multi_monitor_support =
+        $gpu10->display_port =
+        $gpu10->dvi =
+        $gpu10->sli_support =
+        $gpu10->system_requirements =
+        $gpu10->power_connector
+        $gpu10->dual_link_dvi_support =
+        $gpu10->hdcp_ready =
+        $gpu10->save();
+
+        $gpu11 = new \App\Models\Post();
+        $gpu11->model =
+        $gpu11->brand =
+        $gpu11->interface =
+        $gpu11->manufacturer =
+        $gpu11->core_clock =
+        $gpu11->boost_clock =
+        $gpu11->cuda_cores =
+        $gpu11->memory_clock =
+        $gpu11->memory_size =
+        $gpu11->memory_interface =
+        $gpu11->memory_type =
+        $gpu11->api_3d =
+        $gpu11->hdmi =
+        $gpu11->multi_monitor_support =
+        $gpu11->display_port =
+        $gpu11->dvi =
+        $gpu11->sli_support =
+        $gpu11->system_requirements =
+        $gpu11->power_connector
+        $gpu11->dual_link_dvi_support =
+        $gpu11->hdcp_ready =
+        $gpu11->save();
+
+        $gpu12 = new \App\Models\Post();
+        $gpu12->model =
+        $gpu12->brand =
+        $gpu12->interface =
+        $gpu12->manufacturer =
+        $gpu12->core_clock =
+        $gpu12->boost_clock =
+        $gpu12->cuda_cores =
+        $gpu12->memory_clock =
+        $gpu12->memory_size =
+        $gpu12->memory_interface =
+        $gpu12->memory_type =
+        $gpu12->api_3d =
+        $gpu12->hdmi =
+        $gpu12->multi_monitor_support =
+        $gpu12->display_port =
+        $gpu12->dvi =
+        $gpu12->sli_support =
+        $gpu12->system_requirements =
+        $gpu12->power_connector
+        $gpu12->dual_link_dvi_support =
+        $gpu12->hdcp_ready =
+        $gpu12->save();
+
+        $gpu13 = new \App\Models\Post();
+        $gpu13->model =
+        $gpu13->brand =
+        $gpu13->interface =
+        $gpu13->manufacturer =
+        $gpu13->core_clock =
+        $gpu13->boost_clock =
+        $gpu13->cuda_cores =
+        $gpu13->memory_clock =
+        $gpu13->memory_size =
+        $gpu13->memory_interface =
+        $gpu13->memory_type =
+        $gpu13->api_3d =
+        $gpu13->hdmi =
+        $gpu13->multi_monitor_support =
+        $gpu13->display_port =
+        $gpu13->dvi =
+        $gpu13->sli_support =
+        $gpu13->system_requirements =
+        $gpu13->power_connector
+        $gpu13->dual_link_dvi_support =
+        $gpu13->hdcp_ready =
+        $gpu13->save();
+
+        $gpu14 = new \App\Models\Post();
+        $gpu14->model =
+        $gpu14->brand =
+        $gpu14->interface =
+        $gpu14->manufacturer =
+        $gpu14->core_clock =
+        $gpu14->boost_clock =
+        $gpu14->cuda_cores =
+        $gpu14->memory_clock =
+        $gpu14->memory_size =
+        $gpu14->memory_interface =
+        $gpu14->memory_type =
+        $gpu14->api_3d =
+        $gpu14->hdmi =
+        $gpu14->multi_monitor_support =
+        $gpu14->display_port =
+        $gpu14->dvi =
+        $gpu14->sli_support =
+        $gpu14->system_requirements =
+        $gpu14->power_connector
+        $gpu14->dual_link_dvi_support =
+        $gpu14->hdcp_ready =
+        $gpu14->save();
+
+        $gpu15 = new \App\Models\Post();
+        $gpu15->model =
+        $gpu15->brand =
+        $gpu15->interface =
+        $gpu15->manufacturer =
+        $gpu15->core_clock =
+        $gpu15->boost_clock =
+        $gpu15->cuda_cores =
+        $gpu15->memory_clock =
+        $gpu15->memory_size =
+        $gpu15->memory_interface =
+        $gpu15->memory_type =
+        $gpu15->api_3d =
+        $gpu15->hdmi =
+        $gpu15->multi_monitor_support =
+        $gpu15->display_port =
+        $gpu15->dvi =
+        $gpu15->sli_support =
+        $gpu15->system_requirements =
+        $gpu15->power_connector
+        $gpu15->dual_link_dvi_support =
+        $gpu15->hdcp_ready =
+        $gpu15->save();
+
+        $gpu16 = new \App\Models\Post();
+        $gpu16->model =
+        $gpu16->brand =
+        $gpu16->interface =
+        $gpu16->manufacturer =
+        $gpu16->core_clock =
+        $gpu16->boost_clock =
+        $gpu16->cuda_cores =
+        $gpu16->memory_clock =
+        $gpu16->memory_size =
+        $gpu16->memory_interface =
+        $gpu16->memory_type =
+        $gpu16->api_3d =
+        $gpu16->hdmi =
+        $gpu16->multi_monitor_support =
+        $gpu16->display_port =
+        $gpu16->dvi =
+        $gpu16->sli_support =
+        $gpu16->system_requirements =
+        $gpu16->power_connector
+        $gpu16->dual_link_dvi_support =
+        $gpu16->hdcp_ready =
+        $gpu16->save();
+
+        $gpu17 = new \App\Models\Post();
+        $gpu17->model =
+        $gpu17->brand =
+        $gpu17->interface =
+        $gpu17->manufacturer =
+        $gpu17->core_clock =
+        $gpu17->boost_clock =
+        $gpu17->cuda_cores =
+        $gpu17->memory_clock =
+        $gpu17->memory_size =
+        $gpu17->memory_interface =
+        $gpu17->memory_type =
+        $gpu17->api_3d =
+        $gpu17->hdmi =
+        $gpu17->multi_monitor_support =
+        $gpu17->display_port =
+        $gpu17->dvi =
+        $gpu17->sli_support =
+        $gpu17->system_requirements =
+        $gpu17->power_connector
+        $gpu17->dual_link_dvi_support =
+        $gpu17->hdcp_ready =
+        $gpu17->save();
+
+        $gpu18 = new \App\Models\Post();
+        $gpu18->model =
+        $gpu18->brand =
+        $gpu18->interface =
+        $gpu18->manufacturer =
+        $gpu18->core_clock =
+        $gpu18->boost_clock =
+        $gpu18->cuda_cores =
+        $gpu18->memory_clock =
+        $gpu18->memory_size =
+        $gpu18->memory_interface =
+        $gpu18->memory_type =
+        $gpu18->api_3d =
+        $gpu18->hdmi =
+        $gpu18->multi_monitor_support =
+        $gpu18->display_port =
+        $gpu18->dvi =
+        $gpu18->sli_support =
+        $gpu18->system_requirements =
+        $gpu18->power_connector
+        $gpu18->dual_link_dvi_support =
+        $gpu18->hdcp_ready =
+        $gpu18->save();
+
+        $gpu19 = new \App\Models\Post();
+        $gpu19->model =
+        $gpu19->brand =
+        $gpu19->interface =
+        $gpu19->manufacturer =
+        $gpu19->core_clock =
+        $gpu19->boost_clock =
+        $gpu19->cuda_cores =
+        $gpu19->memory_clock =
+        $gpu19->memory_size =
+        $gpu19->memory_interface =
+        $gpu19->memory_type =
+        $gpu19->api_3d =
+        $gpu19->hdmi =
+        $gpu19->multi_monitor_support =
+        $gpu19->display_port =
+        $gpu19->dvi =
+        $gpu19->sli_support =
+        $gpu19->system_requirements =
+        $gpu19->power_connector
+        $gpu19->dual_link_dvi_support =
+        $gpu19->hdcp_ready =
+        $gpu19->save();
+
+        $gpu20 = new \App\Models\Post();
+        $gpu20->model =
+        $gpu20->brand =
+        $gpu20->interface =
+        $gpu20->manufacturer =
+        $gpu20->core_clock =
+        $gpu20->boost_clock =
+        $gpu20->cuda_cores =
+        $gpu20->memory_clock =
+        $gpu20->memory_size =
+        $gpu20->memory_interface =
+        $gpu20->memory_type =
+        $gpu20->api_3d =
+        $gpu20->hdmi =
+        $gpu20->multi_monitor_support =
+        $gpu20->display_port =
+        $gpu20->dvi =
+        $gpu20->sli_support =
+        $gpu20->system_requirements =
+        $gpu20->power_connector
+        $gpu20->dual_link_dvi_support =
+        $gpu20->hdcp_ready =
+        $gpu20->save();
+
+    }
+}
