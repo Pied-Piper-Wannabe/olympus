9a0828958eca0d6359e2b54dbcc8b36d3230ea62
xconstaud <xconstaud@gmail.com>
2017-05-11 17:43:30 -0500

    Update logo and paralax scroll

--- resources/views/builds/create.blade.php
+++ resources/views/builds/create.blade.php
@@ -1,10 +1,17 @@
 @extends('layouts.master')
 
-@section('cover')
-<div class="parallax"></div>
-
 @section('content')
+<div class="parallax">
+  <div class="createCoverText">
+    <h1>Build Your PC</h1>
+  </div>
+</div>
 <div class="container">
+  <div class="row">
+    <div class="col-sm-12 centerText">
+      <p>Need Help? <a href="/builds/tour">Take the tour!</a></p>
+    </div>
+  </div>
   <div class="row">
     
   </div>
