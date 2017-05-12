11e79ffa246295dadc93c0462da36401e91c96f0
gtw117 <gtw117@users.noreply.github.com>
2017-05-12 14:48:46 -0500

    cpu_cooler_table populated

--- database/migrations/2017_05_10_203922_create_cpu_cooler_table.php
+++ database/migrations/2017_05_10_203922_create_cpu_cooler_table.php
@@ -13,7 +13,20 @@ class CreateCpuCoolerTable extends Migration
 	public function up()
 	{
 		Schema::create('cpu_cooler', function(Blueprint $table){
-			$table->
+			$table->string('model');
+			$table->string('brand');
+			$table->string('type');
+			$table->string('block_compatibility');
+			$table->string('block_material');
+			$table->integer('radiator_dimensions');
+			$table->string('radiator_material');
+			$table->integer('fan_size');
+			$table->integer('fan_dimensions');
+			$table->integer('fan_rpm');
+			$table->integer('fan_air_flow');
+			$table->integer('fan_noise');
+			$table->string('tube_material');
+			$table->integer('number_of_fans');
 		});
 	}
 
