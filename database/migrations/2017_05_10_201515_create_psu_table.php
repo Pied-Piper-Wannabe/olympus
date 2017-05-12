a9336e8d4d4c0ad6600a2287e255c105724be4a6
gtw117 <gtw117@users.noreply.github.com>
2017-05-12 14:36:46 -0500

    psu_table populated

--- database/migrations/2017_05_10_201515_create_psu_table.php
+++ database/migrations/2017_05_10_201515_create_psu_table.php
@@ -13,7 +13,23 @@ class CreatePsuTable extends Migration
 	public function up()
 	{
 		Schema::create('psu', function(Blueprint $table){
-			$table->
+			$table->string('model');
+			$table->string('type');
+			$table->string('short_p_n');
+			$table->string('output_capacity');
+			$table->integer('dimensions');
+			$table->string('power_factor_correction');
+			$table->string('power_good_signal'):
+			$table->string('hold_up_time');
+			$table->integer('input_current');
+			$table->integer('input_frequency_range');
+			$table->integer('input_voltage');
+			$table->integer('operating_temp');
+			$table->integer('operating_humidity');
+			$table->string('cooling_system');
+			$table->string('connector');
+			$table->string('efficiency');
+			$table->string('modular');
 		});
 	}
 
