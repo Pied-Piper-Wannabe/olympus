c7c1ce75b060f4146bf6c69da794d30b2c05cff5
gtw117 <gtw117@users.noreply.github.com>
2017-05-12 16:03:28 -0500

    optical_drive_table populated

--- database/migrations/2017_05_10_204459_create_optical_drive_table.php
+++ database/migrations/2017_05_10_204459_create_optical_drive_table.php
@@ -13,7 +13,25 @@ class CreateOpticalDriveTable extends Migration
 	public function up()
 	{
 		Schema::create('optical_drive', function(Blueprint $table){
-			$table->
+			$table->string('model');
+			$table->string('brand');
+			$table->string('type');
+			$table->integer('dvd_rom_access_time');
+			$table->integer('cd_rom_access_time');
+			$table->integer('cache');
+			$table->string('dvd_r');
+			$table->string('dvd_rw');
+			$table->string('cd_r');
+			$table->string('cd_rw');
+			$table->string('dvd_r_dl');
+			$table->integer('dvd_read_speed');
+			$table->integer('cd_read_speed');
+			$table->string('load_type');
+			$table->string('interface');
+			$table->string('os_support');
+			$table->integer('dimensions');
+			$table->string('system_requirements');
+			$table->string('blue_ray');
 		});
 	}
 
