18cda6e4f4ad0dc6267967b2002be22fc76b9357
gtw117 <gtw117@users.noreply.github.com>
2017-05-12 11:33:01 -0500

    ram_table populated

--- database/migrations/2017_05_10_200610_create_ram_table.php
+++ database/migrations/2017_05_10_200610_create_ram_table.php
@@ -13,7 +13,21 @@ class CreateRamTable extends Migration
 	public function up()
 	{
 		Schema::create('ram', function(Blueprint $table){
-			$table->
+			$table->string('model');
+			$table->string('brand');
+			$table->string('series');
+			$table->integer('capacity');
+			$table->string('type');
+			$table->integer('speed');
+			$table->string('cas_latency');
+			$table->string('timing');
+			$table->integer('voltage');
+			$table->string('ecc');
+			$table->string('buffered_registered');
+			$table->string('multi_channel_kit');
+			$table->string('heat_spreader');
+			$table->string('fan_included');
+			$table->integer('number_of_sticks');
 		});
 	}
 
