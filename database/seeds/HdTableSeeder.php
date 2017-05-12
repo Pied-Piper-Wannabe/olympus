393cdc454c3339ece88478beac7a716c805b9ff5
Connor <Csumners17@gmail.com>
2017-05-11 19:00:30 -0500

    Add boilerplate seeder for hard drives

---
+++ database/seeds/HdTableSeeder.php
@@ -0,0 +1,331 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class GpuTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $hd = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd->save();
+
+        $hd1 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd1->save();
+
+        $hd2 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd2->save();
+
+        $hd3 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd3->save();
+
+        $hd4 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd4->save();
+
+        $hd5 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd5->save();
+
+        $hd6 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd6->save();
+
+        $hd7 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd7->save();
+
+        $hd8 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd8->save();
+
+        $hd9 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd9->save();
+
+        $hd10 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd10->save();
+
+        $hd11 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd11->save();
+
+        $hd12 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd12->save();
+
+        $hd13 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd13->save();
+
+        $hd14 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd14->save();
+
+        $hd15 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd15->save();
+
+        $hd16 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd16->save();
+
+        $hd17 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd17->save();
+
+        $hd18 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd18->save();
+
+        $hd19 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd19->save();
+
+        $hd20 = new \App\Models\Post();
+        $hd->model =
+        $hd->brand =
+        $hd->series =
+        $hd->interface =
+        $hd->capacity =
+        $hd->rpm =
+        $hd->cache =
+        $hd->average_latency =
+        $hd->height =
+        $hd->width =
+        $hd->length =
+        $hd->ssd =
+        $hd20->save();
+
+    }
+}
