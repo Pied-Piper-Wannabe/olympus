eb35ca5171ad63143880f5c30a9931eaffa57827
gtw117 <gtw117@users.noreply.github.com>
2017-05-12 11:24:26 -0500

    cpu_table populated

--- database/migrations/2017_05_10_183834_create_cpu_table.php
+++ database/migrations/2017_05_10_183834_create_cpu_table.php
@@ -13,7 +13,25 @@ class CreateCpuTable extends Migration
 	public function up()
 	{
 		Schema::create('cpu', function(Blueprint $table){
-			$table->
+			$table->string('name');
+			$table->string('brand');
+			$table->string('series');
+			$table->string('socket_type');
+			$table->string('core_name');
+			$table->integer('number_of_cores');
+			$table->integer('number_of_threads');
+			$table->integer('operating_frequency');
+			$table->integer('max_turbo_frequency');
+			$table->string('l2_cash');
+			$table->string('l3_cash');
+			$table->string('bit_support');
+			$table->string('hyperthreading_support');
+			$table->string('virtualization_support');
+			$table->string('intergrated_graphics');
+			$table->integer('graphics_base_frequency');
+			$table->integer('max_dynamic_frequency');
+			$table->string('pci_revision');
+			$table->string('pci_lanes');
 		});
 	}
 
