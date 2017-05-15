c04dfae06e77ff72a3c6dcdbff653cd2c7312a72
gtw117 <gtw117@users.noreply.github.com>
2017-05-14 20:43:18 -0500

    modify cpu/gpu/sli/ram/cpu_cooler tables

--- database/migrations/2017_05_10_183834_create_cpu_table.php
+++ database/migrations/2017_05_10_183834_create_cpu_table.php
@@ -13,25 +13,21 @@ class CreateCpuTable extends Migration
 	public function up()
 	{
 		Schema::create('cpu', function(Blueprint $table){
-			$table->string('name');
+			$table->string('model');
 			$table->string('brand');
-			$table->string('series');
 			$table->string('socket_type');
-			$table->string('core_name');
 			$table->integer('number_of_cores');
 			$table->integer('number_of_threads');
+			$table->boolean('hyperthreading_support');
 			$table->integer('operating_frequency');
 			$table->integer('max_turbo_frequency');
-			$table->string('l2_cash');
-			$table->string('l3_cash');
-			$table->string('bit_support');
-			$table->string('hyperthreading_support');
-			$table->string('virtualization_support');
+			$table->string('l1_cache')
+			$table->string('l2_cache');
+			$table->string('l3_cache');
+			$table->string('data_width');
 			$table->string('intergrated_graphics');
-			$table->integer('graphics_base_frequency');
-			$table->integer('max_dynamic_frequency');
-			$table->string('pci_revision');
-			$table->string('pci_lanes');
+			$table->integer('wattage');
+			$table->integer('max_supported_memory');
 		});
 	}
 
@@ -42,6 +38,6 @@ class CreateCpuTable extends Migration
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('cpu');
 	}
 }
