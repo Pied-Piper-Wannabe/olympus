8ec5e85ceb4069bcabc448585be7959ec1c700a7
gtw117 <gtw117@users.noreply.github.com>
2017-05-15 15:01:45 -0500

    modify unsigned()/nullable()

--- database/migrations/2017_05_09_192014_create_builds_table.php
+++ database/migrations/2017_05_09_192014_create_builds_table.php
@@ -19,44 +19,44 @@ class CreateBuildsTable extends Migration
 			$table->integer('user_id')->unsigned();
 			$table->integer('user_id')->references('id')->on('users');
 
-			$table->text('description')->nullable();
+			$table->text('description')->unsigned()->nullable();
 			$table->decimal('price', 5, 2);
 			//------needs a compatibility check-------
 
-			$table->integer('motherboard')->nullable()->unsigned();
+			$table->integer('motherboard')->unsigned();
 			$table->integer('motherboard')->references('part_id')->on('motherbaord');
 
-			$table->integer('cpu')->nullable()->unsigned();
+			$table->integer('cpu')->unsigned();
 			$table->integer('cpu')->references('part_id')->on('cpu');
 
-			$table->integer('gpu')->nullable()->unsigned();
+			$table->integer('gpu')->unsigned();
 			$table->integer('gpu')->references('part_id')->on('gpu');
 
 			$table->boolean('sli');
 
-			$table->integer('ram')->nullable()->unsigned();
+			$table->integer('ram')->unsigned();
 			$table->integer('ram')->references('part_id')->on('ram');
 
-			$table->integer('cpu_cooler')->nullable()->unsigned();
+			$table->integer('cpu_cooler')->unsigned();
 			$table->integer('cpu_cooler')->references('part_id')->on('cpu_cooler');
 
 			//-----------------------------------------
-			$table->integer('hdd')->nullable()->unsigned();
+			$table->integer('hdd')->unsigned();
 			$table->integer('hdd')->references('part_id')->on('hdd');
 
-			$table->integer('psu')->nullable()->unsigned();
+			$table->integer('psu')->unsigned();
 			$table->integer('psu')->references('part_id')->on('psu');
 
-			$table->integer('case')->nullable()->unsigned();
+			$table->integer('case')->unsigned();
 			$table->integer('case')->references('part_id')->on('case');
 
-			$table->integer('optical_drive')->nullable()->unsigned();
+			$table->integer('optical_drive')->unsigned();
 			$table->integer('optical_drive')->references('part_id')->on('optical_drive');
 
-			$table->integer('operating_system')->nullable()->unsigned();
+			$table->integer('operating_system')->unsigned();
 			$table->integer('operating_system')->references('part_id')->on('operating_system');
 
-			$table->string('misc')->nullable()->unsigned();
+			$table->string('misc')->unsigned();
 			$table->string('misc')->references('part_id')->on('misc');
 
 		});
