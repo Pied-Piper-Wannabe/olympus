719505efb01d7311dbabd02fae9724f10e35b94f
Connor <Csumners17@gmail.com>
2017-05-16 13:46:41 -0500

    Add boilerplate for tours table

---
+++ resources/views/tour/tour.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.master')
+@section('content')
+<link rel="stylesheet" type="" href="">
+<h1>Tour</h1>
+<table class="table table-striped">
+  <thead>
+    <th>Part</th>
+    <th>Decription</th>
+  </thead>
+  <tr>
+    <td>CPU</td>
+    <td>...</td>
+  </tr>
+  <tr>
+    <td>GPU</td>
+    <td>...</td>
+  </tr>
+  <tr>
+    <td>Motherboard</td>
+    <td>...</td>
+  </tr>
+  <tr>
+    <td>Hard Drive</td>
+    <td>...</td>
+  </tr>
+  <tr>
+    <td>RAM</td>
+    <td>...</td>
+  </tr>
+  <tr>
+    <td>Optical Drive</td>
+    <td>...</td>
+  </tr>
+  <tr>
+    <td>Power Supply</td>
+    <td>...</td>
+  </tr>
+</table>
+
+
+
+
+@stop
