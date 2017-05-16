f0c2a1d262445b58570a5e1728dca92569e239eb
xconstaud <xconstaud@gmail.com>
2017-05-16 18:44:40 -0500

    Update gpu migration

--- database/migrations/2017_05_10_195646_create_gpu_table.php
+++ database/migrations/2017_05_10_195646_create_gpu_table.php
@@ -14,25 +14,25 @@ class CreateGpuTable extends Migration
 	{
 		Schema::create('gpu', function(Blueprint $table){
 			$table->increments('id');
-			$table->string('model');
+			$table->decimal('price', 5, 2);
 			$table->string('brand');
-			// --------------COMP CHECK--------
+			$table->string('model');
 			$table->string('interface');
-			$table->integer('wattage');
-			// -----------COMP CHECK--------
+			$table->string('chipset');
+			$table->integer('mem_size');
+			$table->string('mem_type');
 			$table->integer('core_clock');
 			$table->integer('boost_clock');
-			$table->integer('cores');
-			$table->boolean('cuda_support');
-			$table->integer('memory_clock');
-			$table->integer('memory_size');
-			$table->string('memory_type');
-			$table->string('api_3d');
+			$table->integer('tdp');
+			$table->string('fan');
+			$table->string('sli_support');
+			$table->string('crossfire_support');
+			$table->integer('length');
+			$table->string('gsync');
+			$table->string('freesync');
+			$table->integer('dvi_d');
+			$table->integer('display_port');
 			$table->integer('hdmi');
-			$table->integer('dvi');
-			$table->boolean('sli_support');
-			$table->boolean('crossfire_support');
-			$table->boolean('gsync');
 		});
 	}
 
