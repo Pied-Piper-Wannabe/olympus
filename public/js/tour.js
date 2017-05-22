cf0f39c4c3ea8458fb380e40bd4a7f73c8cdd8cb
Connor <Csumners17@gmail.com>
2017-05-22 02:28:58 -0500

    Add platform explanation component

--- public/js/tour.js
+++ public/js/tour.js
@@ -6,11 +6,19 @@ ReactDOM.render((
     <Screen>
         <ScreenSlice>
             <div class="cover">
-                <h1 class="killMargin"></h1>
+                <h1 class="killMargin olympus">OLYMPUS</h1>
             </div>
         </ScreenSlice>
         <ScreenSlice>
-            <h1>Second Screen</h1>
+            <div>
+                <h1>So what is Olympus?</h1>
+                <p>Olympus is a website to help you build your next PC.
+                 We check compatibility for you, so you can rest assured when building your computer you wont buy any incorrect parts</p>
+                <p>Never worry about returning parts that don't fit or aren't compatabile with each other again!</p>
+                <p>Building your own PC and need inspiration on where to start?
+                 No problem, our builds section has plenty of pre built PC's to help get you started.</p>
+                <p>Below is an infographic that shows new computer enthusiasts how each piece of hardware in a PC works.</p>
+            </div>
         </ScreenSlice>
         <ScreenSlice>
             <h1>Third Screen</h1>
