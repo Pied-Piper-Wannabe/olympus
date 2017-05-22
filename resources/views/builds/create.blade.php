f333e7415a3808d0383fc1bf0a494a5475667e20
xconstaud <xconstaud@gmail.com>
2017-05-21 21:25:01 -0500

    Update create build page

--- resources/views/builds/create.blade.php
+++ resources/views/builds/create.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.master')
 
 @section('content')
+<div class="ascendParallax">
+  <div class="ascendCoverText">
+    <h1>ASCEND</h1>
+  </div>
+</div>
 <div class="container">
 	<div class="row">
 		<div class="col-md-6 offset-md-3 col-sm-12 offset-sm-0">
+			<h1>Builds:</h1>
+			<h4>Create New Build</h4>
 			<a class="btn btn-primary" href="/builds/new" role="button">Create New</a>
+			<h4>Choose From Your Existing Builds</h4>
 			<div class="dropdown">
 			  <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 			    Edit Existing
@@ -24,3 +32,5 @@
 </div>
 
 @stop
+
+
